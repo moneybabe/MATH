ac7bc82adb3479d611c75fe3b426ee905e848465
Neo Lee <neo.lky852@gmail.com>
2023-05-29 17:48:55 -0700

    181a hw9 initialized

---
+++ MATH181A/hw9/R_script/hw9-1.R
@@ -0,0 +1,27 @@
+# =====================  Data collection ======================================
+# use R to download yahoo finance data and find the simple return using adjusted close price
+# install packages needed to download yahoo finance data
+install.packages("pdfetch") # used to download data
+install.packages("xts") # used to extract "numerical" data from downloaded object
+library(pdfetch)
+library(xts)
+
+# you need to know the stock symbol on yahoo finance. Google them!
+# for Apple, the symbol is AAPL
+AAPL <- pdfetch_YAHOO("AAPL",  # import stock symbol
+                      from = as.Date("2023-01-01"), # choose start date
+                      to = as.Date("2023-05-23"), # choose end date, Sys.Date is system date
+                      interval = "1d" # choose price interval, "1d", "1wk", "1m"
+)
+plot(AAPL)
+                      
+# extract the "numbers" from the downloaded xtc object
+AAPL.aClose <- coredata(AAPL$AAPL.adjclose)
+
+# calculate simple return using r_simp = [p_t - p_(t-1)]/p(t-1)
+# r_simp is simple return, p is adjusted close price, and t is time
+AAPL.simRet = AAPL.aClose[2:length(AAPL.aClose)]/AAPL.aClose[1:length(AAPL.aClose)-1]-1
+plot(AAPL.simRet)
+
+# =====================  Bootstrap CI ======================================
+# adding your code here:
